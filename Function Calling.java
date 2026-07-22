22449213e83d0bc7b61c221c98bd46b52e5d6ade
Dhanush-v18 <dhanushnandhakumar023@gmail.com>
2026-07-22 11:49:20 +0530

    Add Main class with method to print messages

---
+++ Function Calling.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void D () {
+	    System.out.println("D");
+		System.out.println("Hello World");
+	}
+
+ public static void main (String[]args){
+     Main.D();}
+ }
