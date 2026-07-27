09fc0a63a89d7b74de9bdfe16cf5f9d45dc41e0a
Dhanush-v18 <dhanushnandhakumar023@gmail.com>
2026-07-27 12:24:09 +0530

    Implement decimal to binary conversion in Java

---
+++ decimal to binary.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main
+{
+    public static int dectobin(int n){
+        int ans=0;
+        int rem=0;
+        int x=1;
+        while(n>0){
+            rem=n%2;
+            ans=ans+(rem*x);
+            x=x*10;
+            n=n/2;
+        }
+        return ans;
+    }
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int ans=dectobin(a);
+		System.out.println(ans);
+	}
+}
