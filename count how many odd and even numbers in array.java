e7f3dd37b7d841ef566e54b34c8a0daa775128b2
Dhanush-v18 <dhanushnandhakumar023@gmail.com>
2026-07-28 15:38:01 +0530

    Add program to count odd and even numbers in array
    Implement a program to count odd and even numbers in an array.

---
+++ count how many odd and even numbers in array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();      
+        int[] arr = new int[size];    
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        solveproblem(size, arr);
+    }
+    public static void solveproblem(int size, int[] arr) {
+        int evencount = 0, oddcount = 0;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] % 2 == 0) {
+                evencount++;
+            } else {
+                oddcount++;
+            }
+        }
+        System.out.println(evencount);
+        System.out.println(oddcount);
+    }
+}
