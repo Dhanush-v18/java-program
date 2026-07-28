f80c05d8c06b141bbb400e49fd3d64f976fb6fcd
Dhanush-v18 <dhanushnandhakumar023@gmail.com>
2026-07-28 15:54:59 +0530

    Add functionality to find target index in array
    Implement a program to find the index of a target value in an array.

---
+++ find the target value in index in array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int target = sc.nextInt();
+		solveproblem(size, arr, target);
+	}
+	public static void solveproblem(int size, int[] arr, int target) {
+		for (int i = 0; i < size; i++) {
+			if (arr[i] == target) {
+				System.out.println(i);
+				return;
+			}
+		}
+		System.out.println(-1);
+	}
+}
