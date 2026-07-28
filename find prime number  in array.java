24354e33c1226e6d18658c70d964d9ec0f55f401
Dhanush-v18 <dhanushnandhakumar023@gmail.com>
2026-07-28 16:20:31 +0530

    Add prime number finder in array
    Implement a program to find and print prime numbers from an input array.

---
+++ find prime number  in array.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+	public static boolean solveproblem(int num) {
+		if(num<2) {
+			return false;
+		}
+		for(int i=2; i<num; i++) {
+			if(num%i==0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String[]args) {
+		Scanner sc=new Scanner(System.in);
+		int size=sc.nextInt();
+		int[]arr=new int[size];
+		for(int i=0; i<size; i++) {
+			arr[i]=sc.nextInt();
+		}
+		for(int i=0; i<size; i++) {
+			if(solveproblem(arr[i])) {
+				System.out.print(arr[i]);
+			}
+		}
+	}
+}
