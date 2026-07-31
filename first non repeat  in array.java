8ac6984fe9e5604fcd20266cb08de4a2a7fa9597
Dhanush-v18 <dhanushnandhakumar023@gmail.com>
2026-07-31 07:21:22 +0530

    Add first non-repeating element finder
    Implement a method to find the first non-repeating element in an array.

---
+++ first non repeat  in array.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int[]arr=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        firstnonrepeat(size,arr);
+    }
+    public static void firstnonrepeat(int size,int[]arr){
+        for(int i=0;i<size;i++){
+            int count=0;
+            for(int j=0;j<size;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if(count>size/2){
+                System.out.println(arr[i]+" ");
+                return;
+            }
+        }
+        System.out.print(-1);
+    }
+}
